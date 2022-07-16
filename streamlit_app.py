fa245c04ed99b7f3feb45b92913a301248308910
Tyler Zon <tylerzon@yahoo.com>
2022-07-16 13:49:31 -0400

    Changing to see if trade list shows up

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ my_trade_list = pd.read_csv("Accounts_History_TZ.csv")
 symbol = my_trade_list['Symbol']
 symbol_choice = st.sidebar.selectbox('Select which security:', symbol)
 
-years = my_trade_list["Run Date"].loc[my_trade_list["Symbol"]] = symbol_choice
+years = my_trade_list["Run Date"].loc[my_trade_list["Symbol"] == symbol_choice]
 year_choice = st.sidebar.selectbox('', years)
 
 count_row = my_trade_list.shape[0]  # Gives number of rows
 count_col = my_trade_list.shape[1]  # Gives number of columns
 
-my_trade_list = my_trade_list.set_index('Symbol').drop_duplicates()
+my_trade_list = my_trade_list.set_index('Symbol')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 trades_selected = st.multiselect("Pick some trades:", list(my_trade_list.index), ['FAGIX'])
